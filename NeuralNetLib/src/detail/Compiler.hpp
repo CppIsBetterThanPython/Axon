fd0a6ed42b52e351f9d61367dd137efa7feb3e8b
CppIsBetterThanPython <igor.podziewski@gmail.com>
2026-07-22 01:20:37 +0100

    Update compilers

--- NeuralNetLib/src/detail/Compiler.hpp
+++ NeuralNetLib/src/detail/Compiler.hpp
@@ -3,6 +3,8 @@
 #include <Model.hpp>
 #include <graph.hpp>
 
+#include <optional>
+
 class CompilerImpl {
 public:
 	virtual ~CompilerImpl() = default;
@@ -10,28 +12,39 @@ public:
 	virtual ModelTemplate compile(Graph graph) = 0;
 
 	virtual constexpr Platform getPlatform() = 0;
-}
+};
 
-class CompilerCPU : CompilerImpl {
-public:
+class CompilerCPU : public CompilerImpl {
+public:	
+
 	constexpr Platform getPlatform() override {
 		return Platform::CPU;
 	}
 
-	ModelTemplate compiler(Graph graph) override {
-		graph
+	ModelTemplate compile(Graph graph) override {
+
 	}
-}
+};
 
 class Compiler {
 	std::unique_ptr<CompilerImpl> impl;
 public:
-	Compiler(CompilerImpl impl) : impl(impl) {
-	}
+	Compiler(std::unique_ptr<CompilerImpl>& impl) : impl(std::move(impl)) { }
 
-	static std::optional<Compiler> createCompiler(Platform platform)
+	static std::optional<Compiler> createCompiler(Platform platform) {
+		switch (platform) {
+			case Platform::CPU: {
+				std::unique_ptr<CompilerImpl> impl = std::make_unique<CompilerCPU>();
+				return Compiler(impl);
+			}
+			case Platform::OpenCL:
+				return std::nullopt;
+			default:
+				return std::nullopt;
+		}
+	}
 
 	ModelTemplate compile(Graph graph) {
 		return impl->compile(graph);
 	}
-}
+};
